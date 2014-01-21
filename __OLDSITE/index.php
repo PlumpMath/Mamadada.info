2fd3e2c04821ddd05aae57c2696da236c56e1463
Dan Taeyoung <provolot@gmail.com>
2014-01-20 21:41:04 -0800

    added old shit

---
+++ __OLDSITE/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>The Filmballad of MAMADADA</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+
+	<link rel="stylesheet/less" href="css/mamadada.less">
+	<script src="js/less-1.5.0.min.js"></script>
+
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+
+<div class="outer-container">
+  <div class="container ">
+
+	<div class="row centered">
+		<div class="col-md-offset-1 col-md-10 col-video">
+			<div class="js-video [vimeo]"><iframe allowfullscreen="" src="http://player.vimeo.com/video/56660511" frameborder="0" width="500" height="281"></iframe></div>
+		</div>
+		<div class="col-md-offset-1 col-md-5">
+			<h2 class="film-title">The Filmballad of MAMADADA</h2>
+		</div>
+		<div class="col-md-5">
+</p>
+Directed and produced by Lily Benson and Cassandra Guan <br>
+<a href="http://cphdox.dk/en/screening/filmballad-mamadada">World Premiere: November 14, 2013</a><br>
+<a href="mailto:production@mamadada.info">production@mamadada.info</a></p>
+<p>
+More content coming soon. <br> </p>
+		</div>
+
+	</div>
+
+ </div><!-- /.container -->
+
+</div><!-- /.outer-container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-45667824-1', 'mamadada.info');
+    ga('send', 'pageview');
+
+	</script>
+  </body>
+</html>
+
