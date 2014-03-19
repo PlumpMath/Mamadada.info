06f0a39a05dcd65d008baa1aa7e84ce5e496f266
Dan Taeyoung <provolot@gmail.com>
2014-03-18 19:02:42 -0700

    jquery was loaded in no-conflict mode so $ didn't work

--- mamadadainfo/sites/all/themes/mamadada/js/mamadada.js
+++ mamadadainfo/sites/all/themes/mamadada/js/mamadada.js
@@ -1,8 +1,9 @@
-$(document).ready(function() {
+jQuery( document ).ready(function() {
+	console.log("heypo");
 	$(".view-entire-site-view").affix ({
 		offset: {
 			top: 100,
 			bottom: 0
 			}
-		});
+		}); 
 });
